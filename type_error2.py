da594accc615413209fc694fa4f0f50f136ae387
Glenn G. Chappell <ggchappell@alaska.edu>
2026-01-14 11:05:02 -0900

    Add type_error2.py

---
+++ type_error2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# type_error.py
+# Glenn G. Chappell
+# 2026-01-12
+#
+# For CS 331 Spring 2026
+"""Example of type error in Python."""
+
+
+print("Twinkle, twinkle, little car.")
+print("How I wonder what you are.")
+print("Up above the world so high.")
+
+x = 3 + "abc"  # TYPE ERROR! EXECUTION STOPS HERE
+print(x)
+
+print("On a bridge you're zooming by.")
+

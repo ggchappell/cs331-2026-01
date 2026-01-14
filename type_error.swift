6e04832840db23c2c8f7f8a913141ca27de851ee
Glenn G. Chappell <ggchappell@alaska.edu>
2026-01-14 11:04:45 -0900

    Add type_error.swift

---
+++ type_error.swift
@@ -0,0 +1,18 @@
+#!/usr/bin/env swift
+// type_error.swift
+// Glenn G. Chappell
+// 2026-01-12
+//
+// For CS 331 Spring 2026
+// Example of type error in Swift
+
+
+print("Twinkle, twinkle, little car.")
+print("How I wonder what you are.")
+print("Up above the world so high.")
+
+let x = 3 + "abc"  // TYPE ERROR! PROGRAM CANNOT EXECUTE
+print(x)
+
+print("On a bridge you're zooming by.")
+

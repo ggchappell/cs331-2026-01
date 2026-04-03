753679c0c285c4bb654e95e195e87eb51bac2297
Glenn G. Chappell <ggchappell@alaska.edu>
2026-04-03 15:26:00 -0800

    evaluator.lua: FINISH - write func eval

--- evaluator.lua
+++ evaluator.lua
@@ -1,6 +1,7 @@
--- evaluator.lua  UNFINISHED
+-- evaluator.lua
 -- Glenn G. Chappell
--- 2026-04-02
+-- Started: 2026-04-02
+-- Updated: 2026-04-03
 --
 -- For CS 331 Spring 2026
 -- Evaluator for Arithmetic Expression AST (rdparser3.lua format)
@@ -26,8 +27,58 @@ local SIMPLE_VAR = 3
 --
 -- Example of a simple tree-walk interpreter.
 function evaluator.eval(ast, vars)
-    return 42  -- DUMMY
-    -- TODO: WRITE THIS!!!
+    local literal, varname, result, op, arg1_val, arg2_val
+
+    assert(type(ast) == "table")
+    assert(type(vars) == "table")
+
+    assert(#ast >= 2 and #ast <= 3)
+    if ast[1] == NUMLIT_VAL then      -- Numeric literal
+        assert(#ast == 2)
+        literal = ast[2]
+        assert(type(literal) == "string")
+        return tonumber(literal)
+
+    elseif ast[1] == SIMPLE_VAR then  -- Simple variable
+        assert(#ast == 2)
+        varname = ast[2]
+        assert(type(varname) == "string")
+        result = vars[varname]
+        if result == nil then  -- Undefined variable has value 0
+            result = 0
+        end
+        return result
+
+    else                              -- Binary operator
+        assert(#ast == 3)
+        assert(type(ast[1]) == "table")
+        assert(type(ast[2]) == "table")
+        assert(type(ast[3]) == "table")
+        assert(#ast[1] == 2)
+        assert(ast[1][1] == BIN_OP)
+
+        op = ast[1][2]
+        assert(type(op) == "string")
+
+        arg1_val = evaluator.eval(ast[2], vars)
+        assert(type(arg1_val) == "number")
+
+        arg2_val = evaluator.eval(ast[3], vars)
+        assert(type(arg2_val) == "number")
+
+        if op == "+" then
+            return arg1_val + arg2_val
+        elseif op == "-" then
+            return arg1_val - arg2_val
+        elseif op == "*" then
+            return arg1_val * arg2_val
+        elseif op == "/" then
+            return arg1_val / arg2_val  -- Let Lua floating-point
+                                        --  handle div by zero, etc.
+        else
+            assert(false, "Illegal binary operator in AST")
+        end
+    end
 end
 
 
